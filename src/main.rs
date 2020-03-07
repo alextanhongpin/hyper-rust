5c1fcc64c58407f9ec3f745e6b4fa408ab765e10
alextanhongpin <alextan220990@gmail.com>
2020-03-07 14:55:51 +0800

    chore: update code

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,25 @@
-use std::convert::Infallible;
 use std::net::SocketAddr;
+use std::convert::Infallible;
 use hyper::{Body, Request, Response, Server};
-
 use hyper::service::{make_service_fn, service_fn};
 
-async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new("Hello world".into()))
+async fn hello_world(_req: Request<Body>)  -> Result<Response<Body>, Infallible> {
+    Ok(Response::new("hello world".into()))
 }
+
 
 #[tokio::main]
 async fn main() {
-    // We'll bind to 127.0.0.1:3000.
-    let addr = SocketAddr::from(([127,0,0,1], 3000));
-
-    // A `Service` is needed for every connection, so this 
-    // creates one from our `hello_world` function.
-
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    
     let make_svc = make_service_fn(|_conn| async {
         Ok::<_, Infallible>(service_fn(hello_world))
     });
+
     let server = Server::bind(&addr).serve(make_svc);
 
-    // Run this server forever.
-    if let  Err(e) = server.await {
+    println!("listening to server on port *:{}", addr);
+    if let Err(e) = server.await {
         eprintln!("server error: {}", e);
     }
 }
