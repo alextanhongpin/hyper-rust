013911f0e409828c309b51e80c3d6a80ceec6921
alextanhongpin <alextan220990@gmail.com>
2020-03-07 15:16:48 +0800

    chore: update endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,48 @@
-use std::net::SocketAddr;
-use std::convert::Infallible;
-use hyper::{Body, Request, Response, Server};
+use futures::TryStreamExt as _;
+use hyper::{Body, Request, Response, Server, Method, StatusCode };
 use hyper::service::{make_service_fn, service_fn};
 
-async fn hello_world(_req: Request<Body>)  -> Result<Response<Body>, Infallible> {
-    Ok(Response::new("hello world".into()))
+async fn echo(req: Request<Body>)  -> Result<Response<Body>, hyper::Error> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => 
+            Ok(Response::new(Body::from("try POST-ing data from /echo"))) ,
+        (&Method::POST, "/echo") => Ok(Response::new(req.into_body())) ,
+        (&Method::POST, "/echo/uppercase") => {
+            let chunk_stream = req.into_body().map_ok(|chunk| {
+                    chunk.iter()
+                        .map(|byte| byte.to_ascii_uppercase())
+                        .collect::<Vec<u8>>()
+                });
+            Ok(Response::new(Body::wrap_stream(chunk_stream)))
+        },
+        (&Method::POST, "/echo/reverse") => {
+            let full_body = hyper::body::to_bytes(req.into_body()).await?;
+            let reversed = full_body.iter()
+                .rev()
+                .cloned()
+                .collect::<Vec<u8>>();
+            Ok(Response::new(Body::from(reversed)))
+        },
+        _ => {
+            let mut not_found = Response::default();
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
+    }
 }
 
 
 #[tokio::main]
-async fn main() {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>>{
+    let addr = (([127, 0, 0, 1], 3000)).into();
     
-    let make_svc = make_service_fn(|_conn| async {
-        Ok::<_, Infallible>(service_fn(hello_world))
+    let service = make_service_fn(|_conn| async {
+        Ok::<_, hyper::Error>(service_fn(echo))
     });
 
-    let server = Server::bind(&addr).serve(make_svc);
+    let server = Server::bind(&addr).serve(service);
 
-    println!("listening to server on port *:{}", addr);
-    if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
-    }
+    println!("listening to server on port {}", addr);
+    server.await?;
+    Ok(())
 }
